d973535dd06d0b1a091e46ca660806b74acfbe18
dave <dave1@vt.edu>
2017-01-12 18:01:35 -0500

    transition animation

---
+++ public/javascripts/transition.js
@@ -0,0 +1,48 @@
+(function($){
+    var dist = 0.0;
+    var content;
+    var time = 0.0;
+    var speed = 2.0;
+    var nextButton;
+
+    function ani2() {
+        dist = time - 100.0;
+        content.style.marginLeft = dist + "vh";
+    }
+
+    function loop2(){
+        if (time < 95) {
+            ani();
+            time += speed;
+            speed /= 1.05;
+            requestAnimationFrame(loop1);
+        }
+    }
+     
+    function ani() {
+        dist = time - 95.0;
+        content.style.marginLeft = dist + "vh";
+    }
+
+    function loop1(){
+        if (time < 100) {
+            ani();
+            time += speed;
+            speed /= 1.01;
+            requestAnimationFrame(loop1);
+        } 
+    }
+
+    function readyFn(){
+        content = document.getElementsByClassName('main-content')[0];
+        loop1();
+        nextButton = document.getElementsByTagName("button");
+
+        for (i = 0; i < nextButton.length; i++)
+            console.log((nextButton[i]).href );
+        //    setTimeout( function() { window.location = nextButton[i].href }, 500 );
+    }
+    
+    $( document ).ready( readyFn );
+    $( window ).load( readyFn );
+})(jQuery);
