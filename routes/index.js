38554dc67f387ea37660114288de75620dcc6f2c
Dhruv Gupta <dhruv4@gmail.com>
2017-01-12 11:10:44 -0500

    Create routes and pug files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'C1 Credit Card' });
+});
+
+router.get('/customizeCard', function(req, res, next){
+  res.render('customize', { title: 'Customize Card' });
+});
+
+router.get('/judgingYou', function(req, res, next){
+  res.render('judging', { title: 'Judging You' });
+});
+
+router.get('/suggestion', function(req, res, next){
+  res.render('suggestion', { title: 'Suggest a Card' });
 });
 
 module.exports = router;
